c596f1c2b762f5d1b413a1d510f127f01277288f
RAGHAD HAMEDA <hamdehraghad@gmail.com>
2025-11-14 00:17:50 +0100

    Add component Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import icon1 from '../assets/icon-1.svg'
+import Humble from '../assets/Humble.svg'
+import Facebook from '../assets/Facebook.svg'
+import Instagram from '../assets/Instagram.svg'
+import Youtube from '../assets/Youtube.svg'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='domain'>
+            <i className="bi bi-telephone-fill"></i>
+            <p>+46 8 123 122 44</p>
+            <img src={icon1} width="20px" height="19.36px"></img>
+            <p>contact@domain.com</p>
+        </div>
+        <div className='logos'>
+          <img src={Facebook} width="30px" height="30px"></img>
+          <img src={Humble} width="30px" height="30px"></img>
+          <img src={Instagram} width="30px" height="30px"></img>
+          <img src={Youtube} width="30px" height="30px"></img>
+          
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
