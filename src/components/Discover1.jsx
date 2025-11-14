2cd3b66a5e42b117dd61c9402f70648244aa58f0
RAGHAD HAMEDA <hamdehraghad@gmail.com>
2025-11-14 16:13:17 +0100

    Add component Discover1

---
+++ src/components/Discover1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Discover1() {
+  return (
+    <div>
+        <button className='discover-btn1'>Discover More</button>
+    </div>
+  )
+}
+
+export default Discover1
