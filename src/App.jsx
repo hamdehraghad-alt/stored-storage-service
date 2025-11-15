d5ba533ebabbf78ad65176324fe38011d242b099
RAGHAD HAMEDA <hamdehraghad@gmail.com>
2025-11-16 00:55:45 +0100

    Add

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Services from "./views/Services"
 import ContactUs from "./views/ContactUs"
 import Booking from "./views/Booking"
+import NotFound from "./views/NotFound"
 
 
 function App() {
