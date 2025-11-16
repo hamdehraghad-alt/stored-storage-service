f0397fcde988d2ac9cf9463a36f165c8238d4127
RAGHAD HAMEDA <hamdehraghad@gmail.com>
2025-11-16 02:05:14 +0100

    Add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Discover from './Discover.jsx';
+import Discover1 from './Discover1.jsx';
 import Rectangle from '../assets/Rectangle 2.svg';
 
 function Hero() {
@@ -10,10 +10,10 @@ function Hero() {
             <h4>Welcome to StorAid</h4>
             <h1>Space Simplified,<br/>Storage Perfected</h1>
             <p className='paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a sem magna. Etiam ac odio sit<br/> amet lorem fringilla sodales. Cras lobortis, libero at iaculis luctus, nisi ex pellentesque nisi,<br/> at euismod sem ipsum ac dolor.</p>
-            <Discover/>
+            <Discover1/>
         </div>
         <div className='rectangle'>
-            <img src={Rectangle} width="462px" height="534px"></img>
+            <img src={Rectangle} width="462px" height="534px" alt='rec-img'></img>
         </div>
         
       
